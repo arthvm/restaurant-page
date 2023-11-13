36640a858944ddae93e014cef54e3a652a175752
Arthur Mariano <90999445+arthvm@users.noreply.github.com>
2023-11-13 17:10:54 -0300

    Add basic title to the page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import "./style.css";
 
+const handleTitle = function () {
+  const pageTitle = document.createElement("div");
+  pageTitle.setAttribute("id", "title");
+  const firstTitle = document.createElement("p");
+  const secondTitle = document.createElement("p");
+  firstTitle.classList.add("first-title");
+  secondTitle.classList.add("second-title");
+
+  firstTitle.textContent = "Web's";
+  secondTitle.textContent = "Cookies";
+
+  pageTitle.appendChild(firstTitle);
+  pageTitle.appendChild(secondTitle);
+
+  return pageTitle;
+};
+
 const handleMainDivs = (function () {
   const contentDiv = document.getElementById("content");
 
@@ -11,4 +28,9 @@ const handleMainDivs = (function () {
 
   contentDiv.appendChild(headerDiv);
   contentDiv.appendChild(mainDiv);
+})();
+
+const handleHeader = (function () {
+  const headerDiv = document.getElementById("header");
+  headerDiv.appendChild(handleTitle());
 })();
